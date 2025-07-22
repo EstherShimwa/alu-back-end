bd13fa32ce135843c505631be83a5fff5d8df15d
esthershimwa <e.shimwa@alustudent.com>
2025-07-22 13:16:48 +0000

    api

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
 ''' Test request to parse API's
 '''
+
+
 import csv
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1].isdigit():
-        api_endpoint = "https://jsonplaceholder.typicode.com"
-        user_id = sys.argv[1]
+    api_endpoint = "https://jsonplaceholder.typicode.com"
+
+    def get_user_tasks(id):
+        '''Return data for the user id passed as an argument
+        '''
+        user_id = str(id)
         user_data = requests.get(api_endpoint + "/users/" + user_id).json()
         username = user_data.get('username')
         todo_data = \
             requests.get(api_endpoint + "/users/" + user_id + "/todos").\
             json()
-        with open("{}.json".format(user_id), 'w') as json_file:
-            tasks = []
-            for task in todo_data:
-                tasks.append({'task': task['title'],
-                              'completed': task['completed'],
-                              'username': username})
-            data = {"{}".format(user_id): tasks}
-            json.dump(data, json_file)
+        tasks = []
+        for task in todo_data:
+            tasks.append({'username': username,
+                          'task': task['title'],
+                          'completed': task['completed']})
+        data = {"{}".format(user_id): tasks}
+        return data
+
+    all_users = requests.get(api_endpoint + "/users").json()
+    all_json = {}
+    for user in all_users:
+        user_data = get_user_tasks(user.get('id'))
+        all_json.update(user_data)
+    with open("todo_all_employees.json", 'w') as data_file:
+        json.dump(all_json, data_file)
